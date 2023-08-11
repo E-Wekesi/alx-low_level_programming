e1d6be0ffb3b280b73e6df7281415019ac542813
Manu Dev <127873642+E-Wekesi@users.noreply.github.com>
2023-08-11 14:32:29 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char character;
+for (character = 'z'; character >= 'a'; character--)
+{
+putchar(character);
+}
+putchar('\n');
+return (0);
+}
