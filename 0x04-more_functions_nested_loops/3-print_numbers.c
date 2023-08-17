495bf765d779d8c46b48a303681bbec5546a4add
Manu Dev <127873642+E-Wekesi@users.noreply.github.com>
2023-08-17 13:20:47 +0300

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - print 0 to 9
+* return: void
+*/
+void print_numbers(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
