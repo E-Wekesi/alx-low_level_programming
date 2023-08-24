739d24db1f21eee49f9a98fa50cf9ec004b5f6ea
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-24 21:26:44 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,48 +4,48 @@
  * infinite_add - adds two numbers
  * @n1: number one.
  * @n2: number two.
- * @r: buffer that the function will use to store the result.
+ * @r: buffer used to store the result.
  * @size_r: buffer size:
- * Return: the pointer to dest.
+ * Return: Pointer
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int num1 = 0, num2 = 0, operation, big_number, digit1, digit2, sum = 0;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
+	while (*(n1 + num1) != '\0')
+		num1++;
+	while (*(n2 + num2) != '\0')
+		num2++;
+	if (num1 >= num2)
+		big_number = num1;
 	else
-		bg = c2;
-	if (size_r <= bg + 1)
+		big_number = num2;
+	if (size_r <= big_number + 1)
 		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	r[big_number + 1] = '\0';
+	num1--, num2--, size_r--;
+	digit1 = *(n1 + num1) - 48, digit2 = *(n2 + num2) - 48;
+	while (big_number >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
+		operation = digit1 + digit2 + sum;
+		if (operation >= 10)
+			sum = operation / 10;
 		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
+			sum = 0;
+		if (operation > 0)
+		*(r + big_number) = (operation % 10) + 48;
 		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
+			*(r + big_number) = '0';
+		if (num1 > 0)
+			num1--, digit1 = *(n1 + num1) - 48;
 		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
+			digit1 = 0;
+		if (num2 > 0)
+			num2--, digit2 = *(n2 + num2) - 48;
 		else
-			dr2 = 0;
-		bg--, size_r--;
+			digit2 = 0;
+		big_number--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
