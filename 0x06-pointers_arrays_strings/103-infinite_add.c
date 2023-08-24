48e3a350c45a1d6a7ae2077cd6b538125ebbf645
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-24 21:09:15 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,53 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * infinite_add - Adds two numbers
- * @n1: First number as a string
- * @n2: Second number as a string
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
- *
- * Return: Pointer to the result (r)
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, operation, bigger_len, digit1, digit2, sum = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + len1) != '\0')
-		len1++;
-	while (*(n2 + len2) != '\0')
-		len2++;
-	if (len1 >= len2)
-		bigger_len = len1;
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		bigger_len = len2;
-	if (size_r <= bigger_len + 1)
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	r[bigger_len + 1] = '\0';
-	len1--, len2--, size_r--;
-	digit1 = *(n1 + len1) - '0';
-	digit2 = *(n2 + len2) - '0';
-	while (bigger_len >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		operation = digit1 + digit2 + sum;
-		if (operation >= 10)
-			sum = operation / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
-			sum = 0;
-		if (operation > 0)
-			*(r + bigger_len) = (operation % 10) + '0';
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
 		else
-			*(r + bigger_len) = '0';
-		if (len1 > 0)
-			len1--, digit1 = *(n1 + len1) - '0';
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
-			digit1 = 0;
-		if (len2 > 0)
-			len2--, digit2 = *(n2 + len2) - '0';
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
-			digit2 = 0;
-		bigger_len--, size_r--;
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	if (*r == '0')
+	if (*(r) == '0')
 		return (r + 1);
 	else
 		return (r);
