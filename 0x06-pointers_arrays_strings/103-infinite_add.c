52500560c75f3a5e05463fd4e50be0ca0df06dfa
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-24 21:07:38 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,48 +18,37 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		len1++;
 	while (*(n2 + len2) != '\0')
 		len2++;
-
 	if (len1 >= len2)
 		bigger_len = len1;
 	else
 		bigger_len = len2;
-
 	if (size_r <= bigger_len + 1)
 		return (0);
-
 	r[bigger_len + 1] = '\0';
 	len1--, len2--, size_r--;
-
 	digit1 = *(n1 + len1) - '0';
 	digit2 = *(n2 + len2) - '0';
-
 	while (bigger_len >= 0)
 	{
 		operation = digit1 + digit2 + sum;
-
 		if (operation >= 10)
 			sum = operation / 10;
 		else
 			sum = 0;
-
 		if (operation > 0)
 			*(r + bigger_len) = (operation % 10) + '0';
 		else
 			*(r + bigger_len) = '0';
-
 		if (len1 > 0)
 			len1--, digit1 = *(n1 + len1) - '0';
 		else
 			digit1 = 0;
-
 		if (len2 > 0)
 			len2--, digit2 = *(n2 + len2) - '0';
 		else
 			digit2 = 0;
-
 		bigger_len--, size_r--;
 	}
-
 	if (*r == '0')
 		return (r + 1);
 	else
