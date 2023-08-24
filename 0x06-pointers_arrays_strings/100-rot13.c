9c32998d8170033e0f5ca7e0640dac297507fb09
Manu Dev <127873642+E-Wekesi@users.noreply.github.com>
2023-08-24 20:03:20 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,28 @@
 #include "main.h"
-
 /**
- * rot13 -  encoder for rot13.
- * @s: An input string to encode using rot13
- * Return: *s
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
-	int index = 0;
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (s[index] != '\0')
+	while (*(s + count) != '\0')
 	{
-		while ((s[index] >= 'a' && s[index] <= 'z') ||
-				(s[index] >= 'A' && s[index] <= 'Z'))
+		for (i = 0; i < 52; i++)
 		{
-			if ((s[index] >= 'a' && s[index] <= 'm') ||
-					(s[index] >= 'A' && s[index] <= 'M'))
-				s[index] += 13;
-			else
-				s[index] -= 13;
-			index++;
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
 		}
-		index++;
+		count++;
 	}
+
 	return (s);
 }
