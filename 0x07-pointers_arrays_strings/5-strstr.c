446c91c0ff994a4da95127f95258ff8c2ea2bf85
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-28 11:59:43 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,30 +2,29 @@
 
 /**
  * _strstr - finds the first occurrence of the substring.
- * needle in the string haystack.
- * @haystack: entire string.
- * @needle: substring.
- * Return: pointer to the beginning of located substring or
- * NULL if the substring is not found.
+ * @haystack: the entire string to search.
+ * @needle: the substring to locate.
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
 */
 char *_strstr(char *haystack, char *needle)
 {
-	char *bhaystack;
-	char *pneedle;
+	char *begin_haystack;
+	char *ptr_needle;
 
 	while (*haystack != '\0')
 	{
-		bhaystack = haystack;
-		pneedle = needle;
+		begin_haystack = haystack;
+		ptr_needle = needle;
 
-		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		while (*haystack != '\0' && *ptr_needle != '\0' && *haystack == *ptr_needle)
 		{
 			haystack++;
-			pneedle++;
+			ptr_needle++;
 		}
-		if (!*pneedle)
-			return (bhaystack);
-		haystack = bhaystack + 1;
+		if (!*ptr_needle)
+			return (begin_haystack);
+		haystack = begin_haystack + 1;
 	}
 	return (0);
 }
