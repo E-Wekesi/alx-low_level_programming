12ff878b66d2ec03dabf2f0f466c2e8bfbb83f17
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-28 11:49:17 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
- * Return: the number of accepted bytes.
+ * _strspn - calculates the length of a prefix substring.
+ * @s: string to search.
+ * @accept: set of characters.
+ * Return: the length of the prefix substring.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, match_found;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		bool = 1;
+		match_found = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
-				bool = 0;
+				match_found = 0;
 				break;
 			}
 		}
-		if (bool == 1)
+		if (match_found == 1)
 			break;
 	}
 	return (i);
