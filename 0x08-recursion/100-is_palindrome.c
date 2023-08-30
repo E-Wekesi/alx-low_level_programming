9d8c434d0eb902e8b42bdb4ee3e24897aa3f90a6
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-30 21:16:05 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome_helper(char *s, int start, int end)
              return (1);
       if (s[start] != s[end])
              return (0);
-      return is_palindrome_helper(s, start + 1, end - 1);
+      return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int is_palindrome(char *s)
       int length = strlen(s);
       if (length <= 1)
               return (1);
-      return is_palindrome_helper(s, 0, length - 1);
+      return (is_palindrome_helper(s, 0, length - 1));
 }
