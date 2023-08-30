30694af042ca1d2922f0f56c054595849a6735c3
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-30 21:18:30 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,27 +6,27 @@
  * @s: The string to check.
  * @start: The starting index.
  * @end: The ending index.
+ *
  * Return: 1 if the substring is palindrome, otherwise 0.
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-      if (start >= end)
-             return (1);
-      if (s[start] != s[end])
-             return (0);
-      return (is_palindrome_helper(s, start + 1, end - 1));
+    if (start >= end)
+        return (1);
+    if (s[start] != s[end])
+        return (0); 
+    return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
  * is_palindrome - Check if a string is palindrome.
  * @s: The string to check.
- *
  * Return: 1 if 's' is palindrome, otherwise 0.
  */
 int is_palindrome(char *s)
 {
-      int length = strlen(s);
-      if (length <= 1)
-              return (1);
-      return (is_palindrome_helper(s, 0, length - 1));
+    int length = strlen(s);
+    if (length <= 1)
+        return 1;
+    return (is_palindrome_helper(s, 0, length - 1));
 }
