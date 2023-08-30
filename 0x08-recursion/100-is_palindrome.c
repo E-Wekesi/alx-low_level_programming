daf7025feafa61ba6de6ee034d2330cfa50747bd
E-Wekesi <127873642+E-Wekesi@users.noreply.github.com>
2023-08-30 21:33:33 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,25 +14,25 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * comparator - compares each character of the string.
+ * checker - compares each character of the string.
  * @s: string
- * @n1: smallest iterator.
- * @n2: biggest iterator.
+ * @index1: left index.
+ * @index2: right index.
  * Return: .
  */
-int comparator(char *s, int n1, int n2)
+int checker(char *s, int index1, int index2)
 {
-	if (*(s + n1) == *(s + n2))
+	if (*(s + index1) == *(s + index2))
 	{
-		if (n1 == n2 || n1 == n2 + 1)
+		if (index1 == index2 || index1 == index2 + 1)
 			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
+		return (0 + checker(s, index1 + 1, index2 - 1));
 	}
 	return (0);
 }
 
 /**
- * is_palindrome - detects if a string is a palindrome.
+ * is_palindrome - checks if a string is a palindrome.
  * @s: string.
  * Return: 1 if s is a palindrome, 0 if not.
  */
@@ -40,5 +40,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	return (checker(s, 0, _strlen_recursion(s) - 1));
 }
