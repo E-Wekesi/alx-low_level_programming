f520c88331ac7c2fcf2042f1b765e1e4fc71d392
Manu Dev <127873642+E-Wekesi@users.noreply.github.com>
2023-09-13 23:27:18 +0300

    0x0E-structures_typedef

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,27 @@
 #include "function_pointers.h"
 
 /**
-  * int_index - function body
-  * @array: array 
-  * @size: number of arguments
-  * @cmp: input
-  * Return: 0
-  */
+ * int_index - searches for an integer
+ * @array: input integer array.
+ * @size: size of the array.
+ * @cmp: pointer to the function
+ *
+ * Return: index of the first element for which the cmp
+ * function does not return 0. If no elements matches,
+ * return -1. If size <= 0, return -1.
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index = 0;
+	int index;
 
-	if (size > 0)
+	if (array && cmp)
 	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (index < size)
-			{
-				if (cmp(array[index]))
-					return (index);
+		if (size <= 0)
+			return (-1);
 
-				index++;
-			}
-		}
+		for (index = 0; index < size; index++)
+			if (cmp(array[index]))
+				return (index);
 	}
 
 	return (-1);
